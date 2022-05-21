66682396c4076ef852e635e0c9813e8b19904ae3
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-22 01:56:02 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,10 +2,56 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+
 require('dotenv').config()
+
+
 // middle ware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hdvzk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect()
+        // Database Collection  start -------------------
+        const productsCollection = client.db('enaAutomotive').collection('products');
+        const usersCollection = client.db('enaAutomotive').collection('users');
+        // Database Collection  End -------------------
+
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '1d' });
+            // console.log()
+            res.send({ result, token })
+        })
+
+        app.get('/db', (req, res) => {
+            res.send('Inside DB')
+        })
+
+    } finally {
+
+    }
+
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
