53bcfc5749c41af755f18babbefdc48e7e993591
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-22 02:29:57 +0600

    middel terar function added

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hdvzk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded
+        next()
+    });
+}
 
 async function run() {
     try {
@@ -41,8 +55,8 @@ async function run() {
             res.send({ result, token })
         })
 
-        app.get('/db', (req, res) => {
-            res.send('Inside DB')
+        app.get('/db', verifyJWT, (req, res) => {
+            res.send({ response: 'Inside DB' })
         })
 
     } finally {
