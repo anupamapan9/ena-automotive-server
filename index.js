9640c46ff178bb13961eb05967d51eb762274121
ANUPAM MITRA <anupamapan0905@gmail.com>
2022-05-25 09:13:03 +0600

    added successfull delete

--- index.js
+++ index.js
@@ -149,7 +149,12 @@ async function run() {
             res.send(result)
         })
 
-
+        app.delete('/product/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(query)
+            res.send(result)
+        })
         // orders apis -------------------------
         app.post('/order', verifyJWT, async (req, res) => {
             const order = req.body;
